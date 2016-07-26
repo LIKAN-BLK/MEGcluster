a363cca73706c9177ee74db231e98d07335860d3
Bogdan <likan26.09@yandex.ru>
2016-07-26 01:21:51 +0100

    coded LDA + PCA, not tested

--- load_data.py
+++ load_data.py
@@ -5,20 +5,15 @@ from os import listdir
 from os.path import join
 
 def load_data(path):
-    return np.concatenate([extract_grad_mat(join(path,f)) for f in listdir(path) if f.endswith(".mat")],axis=0)
+    mask_rawdata = loadmat('ChannelType.mat')
+    gradiom_mask = np.array(map(lambda x: x[0][0] == 'MEG GRAD',mask_rawdata['Type']))
+    return np.concatenate([extract_grad_mat(join(path,f),gradiom_mask) for f in listdir(path) if f.endswith(".mat")],axis=0)
 
 
-def extract_grad_mat(path):
+def extract_grad_mat(path,gradiom_mask):
     data=loadmat(path)
-    gradiom_mask = parse_labels(data['label'])
-    return np.transpose(data['avg'][gradiom_mask])[np.newaxis,...] #additional dimension for easier concatenation to 3d array in the future
-
-
-def parse_labels(array_to_parse):
-    fun=lambda x: bool(re.match('MEG\d\d\d[2-3]',x[0][0]))
-    return np.array(map(fun,array_to_parse))
-
+    return np.transpose(data['data'][gradiom_mask])[np.newaxis,...] #additional dimension for easier concatenation to 3d array in the future
 
 if __name__== '__main__':
-    data = load_data('..\\meg_data\\em_06_SI')
+    data = load_data('../meg_data/em_06_SI')
     print('It\'s fun!')
--- main.py
+++ main.py
@@ -2,16 +2,61 @@ from load_data import load_data
 from mne.channels import read_ch_connectivity
 from mne.stats import permutation_cluster_test
 import matplotlib.pyplot as plt
+from os.path import join
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn import cross_validation
+from sklearn.metrics import roc_auc_score
+
+def get_data(path):
+    path_to_target = join(path, 'em_06_SI')
+    path_to_nontarget = join(path, 'em_06_error')
+    target_data = load_data(path_to_target)
+    nontarget_data = load_data(path_to_nontarget)
+    return target_data, nontarget_data
+
+def calc_cluster_mask(target_data, nontarget_data):
+    connectivity = read_ch_connectivity('neuromag306planar_neighb.mat', picks=None)
+    X = [target_data, nontarget_data]
+    T_obs, clusters, cluster_p_values, H0 = \
+        permutation_cluster_test(X, n_permutations=1000, connectivity=connectivity[0], check_disjoint=True, tail=0,
+                                 n_jobs=4)
+    return clusters[np.argmin(cluster_p_values)]
+
+def apply_cluster_mask(target_data, nontarget_data,mask):
+    linear_mask = mask.reshape(mask.shape[0]*mask.shape[1])
+    target=target_data.reshape(target_data.shape[0],target_data.shape[1] * target_data.shape[2])[:,np.nonzero(linear_mask)]
+    nontarget=nontarget_data.reshape(nontarget_data.shape[0],nontarget_data.shape[1] * nontarget_data.shape[2])[:,np.nonzero(linear_mask)]
+    return target,nontarget
+
+
+
+def dimension_reduction(target_data,nontarget_data, n_comp=200):
+    pca = PCA(n_components=n_comp)
+    data = np.concatenate((target_data,nontarget_data),axis=0)
+    pca.fit(data)
+    target = pca.fit_transform(target_data)
+    non_target = pca.fit_transform(nontarget_data)
+    return target,non_target
+
+
+def classification(target_data,nontarget_data):
+    clf=LinearDiscriminantAnalysis(solver='eig',shrinkage='auto')
+    X=np.concatenate((target_data,nontarget_data),axis=0)
+    y=np.concatenate([np.ones(target_data.shape[0]),np.zeros(target_data.shape[0])])
+    cross_validation.cross_val_score(clf, X, y,cv=5, scoring='roc_auc')
+
 
 if __name__=='__main__':
-    path_to_target = '..\\meg_data\\em_06_SI'
-    path_to_nontarget = '..\\meg_data\\em_06_error'
+    path_to_target = '../meg_data/em_06_SI'
+    path_to_nontarget = '../meg_data/em_06_error'
     target_data =  load_data(path_to_target)
     nontarget_data =  load_data(path_to_nontarget)
     connectivity = read_ch_connectivity('neuromag306planar_neighb.mat', picks=None)
     X=[target_data,nontarget_data]
     T_obs, clusters, cluster_p_values, H0 = \
-        permutation_cluster_test(X, n_permutations=1000, connectivity=connectivity[0],check_disjoint=True, tail=0, n_jobs=8)
+        permutation_cluster_test(X, n_permutations=1000, connectivity=connectivity[0],check_disjoint=True, tail=0, n_jobs=4)
 
     print('Be happy!')
 
@@ -23,7 +68,6 @@ if __name__=='__main__':
 
     plt.close('all')
     for i_c, c in enumerate(clusters):
-        c = c[0]
         if cluster_p_values[i_c] <= 0.05:
             plt.imshow(c)
             plt.xlabel('channels')
