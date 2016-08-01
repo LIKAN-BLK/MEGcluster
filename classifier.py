3d932c10c3600452bd587096f2163ce73700a2f6
Bogdan <likan26.09@yandex.ru>
2016-08-01 01:23:07 +0100

    OOP way. Not working

--- classifier.py
+++ classifier.py
@@ -1,21 +1,46 @@
 from mne.channels import read_ch_connectivity
 from mne.stats import permutation_cluster_test
 import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
-class Classifier:
+class My_Classifier:
     def __init__(self):
         self.connectivity = read_ch_connectivity('neuromag306planar_neighb.mat', picks=None)
 
-    def _calc_cluster_mask(self,target_data, nontarget_data):
-        X = [target_data, nontarget_data]
-        T_obs, clusters, cluster_p_values, H0 = \
-            permutation_cluster_test(X, n_permutations=1000, connectivity=connectivity[0], check_disjoint=True, tail=0,
-                                 n_jobs=4)
-        return clusters[np.argmin(cluster_p_values)]
+    def _apply_cluster_mask(self,data):
+            #due to geometry troubles returns data in 2d shape (trials x features)
+            linear_mask = self.cluster_mask.reshape(self.cluster_mask.shape[0]*self.cluster_mask.shape[1])
+            data=data.reshape(data.shape[0],data.shape[1] * data.shape[2])[:,np.nonzero(linear_mask)[0]]
+            return data
 
     def fit(self,X,y):
-        cluster_mask = self._calc_cluster_mask(X[y == 1,:,:], X[y == 0,:,:])
-        pca = PCA(n_components=n_comp)
-        data = pca.fit_transform(X)
+        def _calc_cluster_mask(self,target_data, nontarget_data):
+            X = [target_data, nontarget_data]
+            T_obs, clusters, cluster_p_values, H0 = \
+                permutation_cluster_test(X, n_permutations=1000, connectivity=self.connectivity[0], check_disjoint=True, tail=0,
+                                 n_jobs=4)
+            return clusters[np.argmin(cluster_p_values)]
 
-    def predict(self):
+
+        clf=LinearDiscriminantAnalysis(solver='eigen',shrinkage='auto')
+        self.cluster_mask = _calc_cluster_mask(X[y == 1,:,:], X[y == 0,:,:])
+
+        X = self._apply_cluster_mask(self,X)
+        n_comp = min([200,X.shape[0],X.shape[1]])
+        self.pca = PCA(n_components=n_comp)
+        self.pca.fit(X)
+        X = self.pca.transform(X)
+
+        self.clf=LinearDiscriminantAnalysis(solver='eigen',shrinkage='auto')
+        self.clf.fit(X,y)
+
+    def predict(self,X):
+        X = self._apply_cluster_mask(self,X)
+        X = self.pca.transform(X)
+        return self.clf.predict(X)
+    def predict_proba(self,X):
+        X = self._apply_cluster_mask(self,X)
+        X = self.pca.transform(X)
+        return self.clf.predict_proba(X)
+
--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from load_data import load_data
-from mne.channels import read_ch_connectivity
-from mne.stats import permutation_cluster_test
-import matplotlib.pyplot as plt
 from os.path import join
 import numpy as np
-from sklearn.decomposition import PCA
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn import cross_validation
+from sklearn.cross_validation import cross_val_score
 from sklearn.metrics import roc_auc_score
-
+from classifier import My_Classifier
 
 def get_data(path):
     path_to_target = join(path, 'em_06_SI')
@@ -17,60 +12,20 @@ def get_data(path):
     nontarget_data = load_data(path_to_nontarget)
     return target_data, nontarget_data
 
-def calc_cluster_mask(target_data, nontarget_data):
-    connectivity = read_ch_connectivity('neuromag306planar_neighb.mat', picks=None)
-    X = [target_data, nontarget_data]
-    T_obs, clusters, cluster_p_values, H0 = \
-        permutation_cluster_test(X, n_permutations=1000, connectivity=connectivity[0], check_disjoint=True, tail=0,
-                                 n_jobs=4)
-    return clusters[np.argmin(cluster_p_values)]
-
-def apply_cluster_mask(data,mask):
-    #due to geometry troubles returns data in 2d shape (trials x features)
-    linear_mask = mask.reshape(mask.shape[0]*mask.shape[1])
-    data=data.reshape(data.shape[0],data.shape[1] * data.shape[2])[:,np.nonzero(linear_mask)[0]]
-    return data
 
 
 
 def cv_score(target_data,nontarget_data):
+    X = np.concatenate((target_data,nontarget_data),axis=0)
+    y = np.hstack((np.ones(target_data.shape[0]),np.zeros(nontarget_data.shape[0])))
+    my_clf = My_Classifier()
 
-
-    clf=LinearDiscriminantAnalysis(solver='eigen',shrinkage='auto')
-    X=np.concatenate((target_data,nontarget_data),axis=0)
-    y=np.concatenate([np.ones(target_data.shape[0]),np.zeros(target_data.shape[0])])
-    cv = cross_validation.ShuffleSplit(len(y),n_iter=2,test_size=0.5)
-    acc = []
-    auc = []
-    for train_index,test_index in cv:
-        Xtrain = X[train_index, :,:]
-        ytrain = y[train_index]
-
-        Xtest = X[test_index,:]
-        ytest = y[test_index]
-
-        cluster_mask = calc_cluster_mask(Xtrain[ytrain == 1,:,:], Xtrain[ytrain == 0,:,:])
-        Xtrain = apply_cluster_mask(Xtrain, cluster_mask)
-
-
-        pca = PCA(n_components = min([200,Xtrain.shape[1]]))
-        Xtrain = pca.fit_transform(Xtrain)
-        print('Real num components = %d' %(Xtrain.shape[1]))
-
-        clf.fit(Xtrain,ytrain)
-
-        Xtest = apply_cluster_mask(Xtest, cluster_mask)
-        Xtest = pca.transform(Xtest)
-        acc.append(clf.score(Xtest,ytest))
-        auc.append(roc_auc_score(ytest, clf.predict_proba(Xtest)[:,1], average='macro'))
-
-    print np.array(acc).mean()
-    print np.array(auc).mean()
+    scorer = lambda estimator, X, y: roc_auc_score(y,my_clf.predict_proba(X))
+    cross_val_score(estimator=my_clf,X=X,y=y,scoring=scorer,cv=2)
 
 
 if __name__=='__main__':
     path = '../meg_data/'
     target_data, nontarget_data = get_data(path)
-
     cv_score(target_data,nontarget_data)
 
