6ec44f02f98c6d4e7d7d5dc007cae3216f2c924e
Bogdan <likan26.09@yandex.ru>
2016-08-03 19:37:55 +0100

    Multiple classifiers

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn import cross_validation
 from sklearn.metrics import roc_auc_score
+from sklearn.svm import SVC
 
 
 def get_data(path):
@@ -22,7 +23,7 @@ def calc_cluster_mask(target_data, nontarget_data):
     X = [target_data, nontarget_data]
     T_obs, clusters, cluster_p_values, H0 = \
         permutation_cluster_test(X, n_permutations=1000, connectivity=connectivity[0], check_disjoint=True, tail=0,
-                                 n_jobs=4)
+                                 n_jobs=6)
     return clusters[np.argmin(cluster_p_values)]
 
 def apply_cluster_mask(data,mask):
@@ -36,12 +37,19 @@ def apply_cluster_mask(data,mask):
 def cv_score(target_data,nontarget_data):
 
 
-    clf=LinearDiscriminantAnalysis(solver='eigen',shrinkage='auto')
+    eigen_lda=LinearDiscriminantAnalysis(solver='eigen',shrinkage='auto')
+    lsqr_lda=LinearDiscriminantAnalysis(solver='lsqr',shrinkage='auto')
+    svd_lda=LinearDiscriminantAnalysis(solver='svd')
+    C1_lin_svm = SVC(C=1.0,kernel='linear',gamma='auto',probability=True)
+    C1_rbf_svm = SVC(C=1.0,kernel='rbf',gamma='auto',probability=True)
+    C0_5_lin_svm = SVC(C=0.5,kernel='linear',gamma='auto',probability=True)
+    C0_5_rbf_svm = SVC(C=0.5,kernel='rbf',gamma='auto',probability=True)
+    clf_dict = {'eigen_lda':eigen_lda,'lsqr_lda':lsqr_lda,'svd_lda':svd_lda,'C1_lin_svm':C1_lin_svm,'C1_rbf_svm':C1_rbf_svm,'C0_5_lin_svm':C0_5_lin_svm,'C0_5_rbf_svm':C0_5_rbf_svm}
+    auc_dict = {'eigen_lda':np.array([]),'lsqr_lda':np.array([]),'svd_lda':np.array([]),'C1_lin_svm':np.array([]),'C1_rbf_svm':np.array([]),'C0_5_lin_svm':np.array([]),'C0_5_lin_svm':np.array([])}
+
     X=np.concatenate((target_data,nontarget_data),axis=0)
     y=np.concatenate([np.ones(target_data.shape[0]),np.zeros(target_data.shape[0])])
     cv = cross_validation.ShuffleSplit(len(y),n_iter=2,test_size=0.5)
-    acc = []
-    auc = []
     for train_index,test_index in cv:
         Xtrain = X[train_index, :,:]
         ytrain = y[train_index]
@@ -57,16 +65,17 @@ def cv_score(target_data,nontarget_data):
         Xtrain = pca.fit_transform(Xtrain)
         print('Real num components = %d' %(Xtrain.shape[1]))
 
-        clf.fit(Xtrain,ytrain)
+        fit_clf = lambda clf: clf.fit(Xtrain,ytrain)
+        {fit_clf(v) for k,v in clf_dict.items()}
 
         Xtest = apply_cluster_mask(Xtest, cluster_mask)
         Xtest = pca.transform(Xtest)
-        acc.append(clf.score(Xtest,ytest))
-        auc.append(roc_auc_score(ytest, clf.predict_proba(Xtest)[:,1], average='macro'))
 
-    print np.array(acc).mean()
-    print np.array(auc).mean()
+        calc_auc = lambda clf:roc_auc_score(ytest, clf.predict_proba(Xtest)[:,1], average='macro')
+        auc_dict = {k:np.append(v,calc_auc(clf_dict[k])) for k,v in auc_dict.items()}
 
+    auc_dict={k:v.mean() for k,v in auc_dict.items()}
+    print auc_dict
 
 if __name__=='__main__':
     path = '../meg_data/'
