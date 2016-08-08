69489aac83eb22525eff3276447cdf906a391e4e
Bogdan <likan26.09@yandex.ru>
2016-08-08 21:25:22 +0100

    Addded cluster script

---
+++ MEGcluster.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#SBATCH -D /s/ls2/u-sw/likan_blk/MEGcluster
+#SBATCH -o out/%j.out
+#SBATCH -e out/%j.err
+#SBATCH -t 02:30:00
+#SBATCH -p hpc2-16g-3d
+#SBATCH --cpus-per-task 8
+
+module load mpi
+$MPIRUN /s/ls2/u-sw/likan_blk/anaconda2/bin/python2.7 main.py | tee out/run.log."$SLURM_JOBID"
