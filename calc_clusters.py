5b5ae7766ff1b08c2bf57f8caad4b4a78f466193
LIKAN <likan26.09@yandex.ru>
2016-08-16 22:20:28 +0300

    Fixes in calc_cluster

--- calc_clusters.py
+++ calc_clusters.py
@@ -10,9 +10,9 @@ def search_clusters(target_data, nontarget_data):
     T_obs, clusters, cluster_p_values, H0 = \
         permutation_cluster_test(X, n_permutations=1500, connectivity=connectivity[0], check_disjoint=True, tail=0,
                                  n_jobs=8,verbose=True)
-    ind = sorted(range(len(cluster_p_values)), key=lambda k: cluster_p_values[k])[:6]
-    cluster_sizes = [cluster.size() for cluster in clusters[ind]]
-    res = dict(zip(cluster_p_values[ind],cluster_sizes))
+    indexes = sorted(range(len(cluster_p_values)), key=lambda k: cluster_p_values[k])[:6]
+    cluster_sizes = [clusters[ind].sum() for ind in indexes]
+    res = zip(cluster_p_values[indexes],cluster_sizes)
     print(res)
     return res
 
@@ -23,7 +23,7 @@ def process_dirs(path_to_experiments):
     result = map(pipe,dirs)
     for i,dir in enumerate(dirs):
         print('%s:' % dir)
-        for entry in result[i].items():
+        for entry in result[i]:
             print '  {0}:{1}'.format(*entry)
 
 if __name__ == '__main__':
