6da5885a0db32ce56017098099c06b1a53be2199
LIKAN <likan26.09@yandex.ru>
2016-09-08 16:31:44 +0300

    Prolonged time

--- MEGcluster.sh
+++ MEGcluster.sh
@@ -2,7 +2,7 @@
 #SBATCH -D /s/ls2/u-sw/likan_blk/MEGcluster
 #SBATCH -o out/%j.out
 #SBATCH -e out/%j.err
-#SBATCH -t 02:30:00
+#SBATCH -t 03:00:00
 #SBATCH -p hpc2-16g-3d
 #SBATCH --cpus-per-task 8
 
